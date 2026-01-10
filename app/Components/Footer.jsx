b456dcd4cf3624859af4a5f72376a932dea980bb
Omar-Assem <161471312+Omar-Assem@users.noreply.github.com>
2026-01-10 15:24:47 +0200

    updates

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -9,27 +9,20 @@ const Footer = () => {
       <div className="relative z-10 flex flex-row items-center justify-center py-10  text-white">
         <div className="relative z-10 flex flex-col items-center justify-center py-10  text-white">
           <span className="text-3xl mb-5"> Quick Links</span>
-          {
-            DFooter.OuickLink.map((e, i) => (
-              <div key={i}>
-                <Link href={e.url}>{e.title}</Link>
-              </div>
-            ))
-          }
+          {DFooter.OuickLink.map((e, i) => (
+            <div key={i}>
+              <Link href={e.url}>{e.title}</Link>
+            </div>
+          ))}
         </div>
         <div className="relative z-10 flex flex-col items-center justify-center py-10  text-white">
           <span className="text-3xl mb-5">Office Location</span>
-          {
-            DFooter.OfficeLocation.map((e, i) => (
-              <div
-                className="flex flex-row justify-center items-center"
-                key={i}
-              >
-                <span className="text-xl">{e.icons}</span>
-                <span className="text-xl">{e.title}</span>
-              </div>
-            ))
-          }
+          {DFooter.OfficeLocation.map((e, i) => (
+            <div className="flex flex-row justify-center items-center" key={i}>
+              <span className="text-xl">{e.icons}</span>
+              <span className="text-xl">{e.title}</span>
+            </div>
+          ))}
         </div>
       </div>
     </footer>
